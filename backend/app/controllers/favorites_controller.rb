c5b3976492ef25168b9c5f713641e33868cd8559
github 26krkz <26krkz@gmail.com>
2021-03-27 23:35:57 +0900

    お気に入り追加機能の実装

--- backend/app/controllers/favorites_controller.rb
+++ backend/app/controllers/favorites_controller.rb
@@ -1,13 +1,17 @@
 class FavoritesController < ApplicationController
-  
-  # お気に入り登録
+
+  # お気に入りに追加
   def create
     @favorite = Favorite.create(user_id: params[:user_id], video_id: params[:video_id])
+    render json: { message: 'お気に入りに追加しました！'}
   end
   # お気に入り削除
   def destroy
-    @favorite = Favorite.find_by(user_id: params[:user_id], video_id: params[:video_id])
-    @favorite.destroy
+    current_user
+    select_video
+    # @favorite = Favorite.find_by(user_id: @current_user.id, video_id: params: @video.id)
+    # @favorite.destroy
+    render json: { message: 'お気に入りから削除しました', current_user: @current_user, video: @video }
   end
 
 end
--- frontend/src/components/Youtube.vue
+++ frontend/src/components/Youtube.vue
@@ -1,6 +1,6 @@
 <template>
 <v-expand-transition>
-  <v-container v-show="expand" class="container grey lighten-2">
+  <v-container v-show="expand" class="container grey lighten-3">
       <v-btn class="close-btn" color="black" icon small outlined @click="expand = !expand">
           <v-icon>mdi-window-close</v-icon>
       </v-btn>
@@ -12,7 +12,7 @@
         </v-col>
         <v-col cols='2'>
 
-            <v-btn v-show="show1" block @click="addFavorite">
+            <v-btn v-show="show1" block @click="pressFavoriteBtn">
                 お気に入りに追加
                 <v-icon v-bind:color="heartColor">{{ heartIcon }}</v-icon>
             </v-btn>
@@ -32,7 +32,7 @@
             <div class="select-btn">
             <!-- 再生時間を設定 -->
             <label for="playing">再生時間：
-            <select id="playing" v-model="playingTime" v-on:change="EndTime()">
+            <select id="playing" v-model="playingTime" v-on:change="getEndTime">
                 <option selected>指定しない</option>
                 <option v-for="pTime in pTimes" v-bind:key="pTime.id" v-bind:value="pTime.value">
                     {{ pTime.text }}
@@ -41,7 +41,7 @@
             </label>
             </div>
 
-            <v-btn block class="resume-btn" @click="Resume">表示</v-btn>
+            <v-btn block class="resume-btn" @click="resume">表示</v-btn>
         </v-col>
         <v-col class="iframe" cols='6'>
             <iframe width="560" height="315" 
@@ -50,6 +50,18 @@
             <div class="iframe-cover" v-show="show2">表示ボタンを押してね</div>
         </v-col>
       </v-row>
+      <!-- お気に入りに加えたらスナックバーを表示する -->
+    <v-snackbar
+      class="snackbar"
+      v-model="snackbar" 
+      color="cyan"
+      elevation="7"
+      transition="scroll-y-reverse-transition"
+      timeout="1500"
+      absolute
+    >
+      <span>{{ snackbarText }}</span>
+    </v-snackbar>
   </v-container>
 </v-expand-transition>
 </template>
@@ -68,6 +80,8 @@ export default {
           show2: true,
           heartColor: null,
           heartIcon: "mdi-heart-outline",
+          snackbar: false,
+          snackbarText: '',
           selectedMovieUrl: '',
           startTime: '',
           playingTime: '',
@@ -106,6 +120,9 @@ export default {
     //   マップ上のマーカーが押されpropsで取得したvideoIdが更新されるたびに以下を実行
       videoId: function(){
                     let that = this;
+                    //一度お気に入りボタンのハートを透明にする。つまりマップのマーカーを押すたびにハートを透明にする。
+                    this.heartIcon = "mdi-heart-outline";
+                    this.heartColor = null;
 
                     //    videoIdを元にdb上のvideoテーブルからvideoIdと一致するvideo_idをもつデータを取得
                     //    データ取得後、propsで取得したyoutubeのプレイリストの動画の中から取得したデータと一致する動画をitemに格納
@@ -114,7 +131,9 @@ export default {
                     axios.get(url, {withCredentials: true} )
                     .then(function (response) {
                         that.videoData = response.data;
-                        that.whetherAddedFavorite();
+                        if( that.current_user != null){
+                            that.whetherAddedFavorite();
+                        }
                     })
                     .catch(function (error) {
                         console.log(error);
@@ -128,20 +147,21 @@ export default {
   },
   methods: {
     // 開始時間と再生時間の和を終了時間としてendTimeに代入している
-    EndTime(){
+    getEndTime(){
             this.endTime = Number(this.startTime) + Number(this.playingTime);
     },
-    Resume(){
+    resume(){
         this.selectedMovieUrl= 'https://www.youtube.com/embed/' + this.item.snippet.resourceId.videoId
                                 + '?start=' + this.startTime
                                 + '&end=' + this.endTime;
 
-        if(this.show == true){this.show = false;}  //初めて表示ボタンを押す時には.iframe-coverを外す。
+        if(this.show2 == true){this.show2 = false;}  //初めて表示ボタンを押す時には.iframe-coverを外す。
     },
     whetherAddedFavorite(){
          // ログインしているユーザーがお気に入りした全ての動画のvideo_idを取得し、その中に選択した動画のidがあるか調べる。
          // あればお気に入り登録されているのでハートマークを赤色に、なければそのままにする。
         let that = this;
+        console.log(that.videoData.id);
         axios.get('http://localhost/users/favorites', {withCredentials: true} )
         .then(function (response) {
             let favoriteList = response.data;
@@ -149,9 +169,6 @@ export default {
                 if(favoriteList[i].id == that.videoData.id){
                     that.heartIcon = "mdi-heart";
                     that.heartColor = "pink";
-                }else{
-                    that.heartIcon = "mdi-heart-outline";
-                    that.heartColor = null;
                 }
             }
         })
@@ -159,15 +176,57 @@ export default {
             console.log(error);
         })
     },
-    addFavorite(){
+    pressFavoriteBtn(){
       if(this.heartIcon == "mdi-heart-outline"){
           this.heartIcon = "mdi-heart";
           this.heartColor = "pink";
+        //   this.addFavorite();
       }else{
           this.heartIcon = "mdi-heart-outline";
           this.heartColor = null;
+          this.removeFavorite();
       }
     },
+    addFavorite(){
+        //お気に入りに登録したら、ログインしているユーザーとその動画をaxios.postによりfavoritesテーブルに登録する
+        let that = this;
+        const url = 'http://localhost/favorite';
+        let params = {user_id: this.current_user.id,
+                      video_id: this.videoData.id
+                      };
+        axios.post( url, params, { withCredentials: true })
+        .then(function (response) {
+            that.snackbarText = response.data.message;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })    
+        //snackbarを表示。表示して消えた後、再びsnackbarをfalseにセットする
+        this.snackbar = true;
+        setTimeout(function(){
+            that.snackbar = false;
+        }, 2000);
+    },
+    removeFavorite(){
+        //既にお気に入りに登録している場合、favoritesテーブルのuser_idとvideo_idの一致するデータを削除する。
+        let that = this;
+        const url = 'http://localhost/favorite';
+        // let params = {data: { video_id: this.videoData.id }};
+        // console.log(params)
+        axios.delete( url, { withCredentials: true })
+        .then(function (response) {
+            that.snackbarText = response.data.message;
+            console.log(response.data)
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        //snackbarを表示。表示して消えた後、再びsnackbarをfalseにセットする
+        this.snackbar = true;
+        setTimeout(function(){
+            that.snackbar = false;
+        }, 2000);
+    }
   }
 }
 
