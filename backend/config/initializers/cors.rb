a9b6f579086fc100c1b800833a2a5fa7e674e703
github 26krkz <26krkz@gmail.com>
2021-04-22 23:56:38 +0900

    netlify.tomlの設定

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,12 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'http://localhost:8080', 'https://virtualwalk.herokuapp.com'
+    # origins 'http://localhost:8080'
     origins '*'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false
+      credentials: true
   end
 end
