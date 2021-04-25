08ef22cb55d35c86a13be3c26868a11a52d1ccbe
github 26krkz <26krkz@gmail.com>
2021-04-25 17:49:55 +0900

    master keyの変更

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -8,7 +8,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:8080', 'https://virtualwalk.netflify.app'
-    # origins '*'
 
     resource '*',
       headers: :any,
