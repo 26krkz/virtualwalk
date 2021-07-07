430d68bc0eb9ad38c1612585b946778d9ef625d3
github 26krkz <26krkz@gmail.com>
2021-07-07 17:32:54 +0900

    環境構築の修正

--- backend/config/unicorn.rb
+++ backend/config/unicorn.rb
@@ -1,16 +1,16 @@
 #docker-composeを使った時の開発環境での設定
-# $worker  = 2
-# $timeout = 30
+$worker  = 2
+$timeout = 30
 $app_dir = "/webapp"
 $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
 $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
 # set config
-worker_processes  2
+worker_processes  $worker
 working_directory $app_dir
 stderr_path $std_log
 stdout_path $std_log
-timeout 30
+timeout $timeout
 listen  $listen
 pid $pid
 # loading booster
